36c97798d1d9236538d0ede3a4628ce110e276bb
Rohan Siddhu <48413269+RohanSiddhu@users.noreply.github.com>
2026-05-05 10:33:50 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+// Brute Force
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
