7fa60fb35a9a3b012463af258c88f9f428252290
Rohan Siddhu <48413269+RohanSiddhu@users.noreply.github.com>
2026-05-07 11:57:39 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.cpp
@@ -0,0 +1,19 @@
+// Vertical Scanning
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        std::size_t j{0};
+
+        for (std::size_t i{0}; i < strs[0].length(); i++) {
+            for (const auto & str : strs) {
+                if (str.length() == j || str[j] != strs[0][j]) {
+                    return strs[0].substr(0, j);
+                }
+            }
+            ++j;
+        }
+
+        return strs[0].substr(0, j);
+    }
+};
